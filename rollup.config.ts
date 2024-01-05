ffb94169703160babb983d29f6fa02845c120c78
xiaoxiyao <xiyao.luoxiao@gmail.com>
2024-01-06 00:13:53 +0800

    fix: module resolution error

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,25 +1,65 @@
 import typescript from '@rollup/plugin-typescript'
-import type { RollupOptions } from 'rollup'
+import type { Plugin, RollupOptions } from 'rollup'
+import { dts } from 'rollup-plugin-dts'
+import { rimraf } from 'rimraf'
 
-export default {
-  input: './src/index.ts',
-  output: [
-    {
-      format: 'cjs',
-      sourcemap: true,
-      file: 'dist/bundle.cjs.js',
+export default [
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        format: 'cjs',
+        sourcemap: true,
+        file: 'dist/bundle.cjs',
+      },
+      {
+        format: 'es',
+        sourcemap: true,
+        file: 'dist/bundle.js',
+      },
+    ],
+    plugins: [
+      cleanBefore(),
+      typescript({
+        declaration: true,
+        rootDir: 'src',
+        declarationDir: 'dist/types',
+      }),
+    ],
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [
+      {
+        file: 'dist/index.d.cts',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.d.ts',
+        format: 'es',
+      },
+    ],
+    plugins: [
+      dts(),
+      cleanEnd(),
+    ],
+  },
+] satisfies Array<RollupOptions>
+
+function cleanBefore(): Plugin {
+  return {
+    name: 'cleanBefore',
+    buildStart: async () => {
+      await rimraf('dist')
     },
-    {
-      format: 'es',
-      sourcemap: true,
-      file: 'dist/bundle.esm.js',
+  }
+}
+
+function cleanEnd(): Plugin {
+  return {
+    name: 'cleanEnd',
+    writeBundle: async () => {
+      await rimraf('dist/types')
     },
-  ],
-  plugins: [
-    typescript({
-      declaration: true,
-      rootDir: 'src',
-      declarationDir: 'dist/types',
-    }),
-  ],
-} satisfies RollupOptions
+  }
+}
